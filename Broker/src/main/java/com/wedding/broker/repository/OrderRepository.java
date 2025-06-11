777934029c1eda8310cb2510c0ed8e9fb9e80993
Ranger7899 <ymasad2003@gmail.com>
2025-06-11 18:15:02 +0200

    fully integrated the broker DB with user table and manager table

--- Broker/src/main/java/com/wedding/broker/repository/OrderRepository.java
+++ Broker/src/main/java/com/wedding/broker/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.wedding.broker.repository;
 
 import com.wedding.broker.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository; // Change to JpaRepository
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> { // Change ID type to Long
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    // Spring Data JPA automatically provides methods like save(), findById(), findAll(), delete()
+
+    // NEW: Find all orders by a specific user ID
+    List<Order> findByUserId(String userId);
 }
